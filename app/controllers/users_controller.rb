8d61b03dcf01877c74dc54079b4d10bdf7ccb92e
Gabe Kane <gabrielkane@gmail.com>
2016-06-03 16:14:16 +0000

    Update Users Ui and form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,22 @@ class UsersController < ApplicationController
       redirect_to articles_path
 
     else
-
       render 'new'
-
     end
-
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account information has been updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
 private
